3b5c5efe3e837fc9fe34c1c01c7f6e9054b46df8
Galber390 <dark74657@gmail.com>
2025-02-26 09:08:06 -0600

    Hice cambios esteticos meti el menu en un ciclo y agregue la opcion para salir del ciclo

--- MN.java
+++ MN.java
@@ -9,39 +9,51 @@ public class MN {
         Scanner leer = new Scanner(System.in);
     
         //Menu
+        do {
         System.out.println("==========");
         System.out.println("Bienvenido || Seleccione una opcion:");
         System.out.println("1.-Almacenamiento ");
         System.out.println("2.-Ventas");
         System.out.println("3.-Compras");
+        System.out.println("4.Salir");
         System.out.println("==========");
 
         Opcion = leer.nextInt();
         //almacenamiento
         switch(Opcion){
         case 1: System.out.println("Productos ");
-        System.out.println("==========");
+        System.out.println("========================");
         System.out.println("SELECCIONE UNA OPCION:");
         System.out.println("1.-REGISTRAR PRODUCTO");
         System.out.println("2.-VER");
         System.out.println("3.- QUITAR");
+        System.out.println("========================");
          // 
         Opcion = leer.nextInt();
         switch(Opcion){
-        case 1:  System.out.println("================");
+        case 1: System.out.println("================================");
                 System.out.println("INGRESE EL PRODUCTO A REGISTRAR:");
-                REG = leer.nextInt();
+                System.o;
         }
         break;
+
         case 2: System.out.println("Te veo al rato");
         break;
-        default: System.out.println("oPCION Invalida");
+        default: System.out.println("Opcion Invalida");
 
         Opcion = leer.nextInt();
         switch(Opcion){
-            case 1:
+            case 1:;
             break;
         }
+        switch(Opcion){
+            case 3:;
+            break;
+        }
+        case 4: System.out.println("Cerrando programa");
+        break;
         }
     }
+    while (Opcion !=4);
+}
 }
