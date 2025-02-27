f9fb193c6320403dd0c25b89b91266b509b8095a
Galber <dark74657@gmail.com>
2025-02-26 19:08:37 -0600

    Hice que la parte del registrar productos funcione agrege un default para el primer menu

--- MN.java
+++ MN.java
@@ -12,48 +12,66 @@ public class MN {
         do {
         System.out.println("====================================");
         System.out.println("Bienvenido || Seleccione una opcion:");
-        System.out.println("1.-Almacenamiento ");
+        System.out.println("1.-Inventario");
         System.out.println("2.-Ventas");
         System.out.println("3.-Compras");
-        System.out.println("4.Salir");
+        System.out.println("4.-Salir");
         System.out.println("====================================");
-
+        System.out.println(" ");
         Opcion = leer.nextInt();
         //almacenamiento
         switch(Opcion){
-        case 1: System.out.println("Productos ");
-        System.out.println("========================");
-        System.out.println("SELECCIONE UNA OPCION:");
+        case 1:
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.println("======================================");
+        System.out.println("Esta en el inventario que desea hacer ");
         System.out.println("1.-REGISTRAR PRODUCTO");
         System.out.println("2.-VER");
-        System.out.println("3.- QUITAR");
-        System.out.println("========================");
+        System.out.println("3.-QUITAR");
+        System.out.println("======================================");
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.println(" ");
          // 
         Opcion = leer.nextInt();
         switch(Opcion){
         case 1: System.out.println("================================");
-                System.out.println("INGRESE EL PRODUCTO A REGISTRAR:");
+                System.out.println("Cuantos productos va a registrar");
+                REG = leer.nextInt();
 
-        }
+                String[] Productos = new String [REG];
+                int[] Cantidad = new int [REG];
+
+                for (int i = 0; i < REG; i++){
+
+                System.out.println("Ingrese el nombre del producto:");
+                Productos [i] = leer.nextLine();
+                Productos [i] = leer.nextLine();
+
+                System.out.println("Ingrese la cantidad de Productos:");
+                Cantidad [i] = leer.nextInt();
+                }
+                for (int i = 0; i<REG; i++){
+                System.out.println("Se registro el producto " + Productos[i] + " con " + Cantidad[i] + " unidades");
+                }
         break;
 
-        case 2: System.out.println("Te veo al rato");
+        case 2: 
         break;
-        default: System.out.println("Opcion Invalida");
 
-        Opcion = leer.nextInt();
-        switch(Opcion){
-            case 1:;
-            break;
-        }
-        switch(Opcion){
-            case 3:;
-            break;
-        }
-        case 4: System.out.println("Cerrando programa");
+        case 3:
         break;
+
+        default: System.out.println("Opcion invalida");
+
         }
-    }
-    while (Opcion !=4);
+        default: System.out.println("Opcion invalida");
+        }
+        }
+while (Opcion !=4); System.out.println("Cerrando programa");
 }
 }
+
+
